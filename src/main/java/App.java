b8262f3b567ac0822d776d407249464b2a9683b4
Mikhail-Lykov <Mikhail-Lykov@users.noreply.github.com>
2020-09-07 15:38:58 +0400

    Complete Task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
+        System.out.println(bean1.getMessage());
+
+        Cat catBean1 = applicationContext.getBean(Cat.class);
+        Cat catBean2 = applicationContext.getBean(Cat.class);
+        System.out.println("Равны ли ссылки HelloWorld? : " + bean1.equals(bean2));
+        System.out.println("Равны ли ссылки Cat? : " + catBean1.equals(catBean2));
     }
 }
